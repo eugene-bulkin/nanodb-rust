c1c3240c51045bbfbda8c862640ce42b4e2e7cb4
Eugene Bulkin <ebulkin@caltech.edu>
2017-02-24 18:26:32 -0800

    Add logging crates and set up logger in binary

--- src/bin.rs
+++ src/bin.rs
@@ -1,8 +1,23 @@
 extern crate nanodb;
+#[macro_use]
+extern crate log;
+extern crate fern;
 
 use nanodb::Client;
 
 fn main() {
+    let logger_config = fern::DispatchConfig {
+        format: Box::new(|msg: &str, level: &log::LogLevel, location: &log::LogLocation| {
+            format!("[{}][{}] {}", location.module_path(), level, msg)
+        }),
+        output: vec![fern::OutputConfig::stdout()],
+        level: log::LogLevelFilter::Trace,
+    };
+
+    if let Err(e) = fern::init_global_logger(logger_config, log::LogLevelFilter::Trace) {
+        panic!("Failed to initialize global logger: {}", e);
+    }
+
     let mut client = Client::new();
 
     client.run();
--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@ extern crate tempdir;
 extern crate byteorder;
 #[macro_use]
 extern crate lazy_static;
+#[macro_use]
+extern crate log;
 
 pub mod parser;
 pub mod storage;
